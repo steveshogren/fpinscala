e7657c407917a8bb147e04233c7f44b060b999ae
Jack <steve.a.shogren@gmail.com>
2016-02-26 07:04:27 -0500

    Exercise 2.2

--- src/main/scala/hof.scala
+++ src/main/scala/hof.scala
@@ -1,6 +1,17 @@
 package hof
 
 object HOF {
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def g(rest: Array[A], a: A, b: A) : Boolean = {
+      var o :Boolean = ordered(a,b)
+      if (rest.length == 0 || rest.length == 1) o
+      else o && g(rest.drop(1), b, rest(0))
+    }
+    g(as.drop(1), as(0), as(1))
+  }
+
   def fib(n: Int): Int = {
     @annotation.tailrec
     def go(n: Int, a: Int, b: Int) : Int =
@@ -8,4 +19,5 @@ object HOF {
       else go(n-1, b, a+b)
     go(n, 0, 1)
   }
+
 }
--- src/test/scala/main.scala
+++ src/test/scala/main.scala
@@ -7,8 +7,27 @@ import hof.HOF
 import collection.mutable.Stack
 import org.scalatest._
 
-class HOFSpec extends FlatSpec with Matchers {
+class HW2P2 extends FlatSpec with Matchers {
+  // Implement isSorted , which checks whether an Array[A] is sorted according to a
+  // given comparison function:
+  // def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean
+  "isSorted" should "prove 1,2 sorted" in {
+    HOF.isSorted(Array[Int](1,2), (a:Int, b:Int) => { a < b}) should be (true)
+  }
+  "isSorted" should "prove 2,1,4 not sorted" in {
+    HOF.isSorted(Array[Int](2,1,3), (a:Int, b:Int) => { a < b}) should be (false)
+  }
+  "isSorted" should "prove 1,2,1 not sorted" in {
+    HOF.isSorted(Array[Int](1, 2, 1), (a:Int, b:Int) => { a < b}) should be (false)
+  }
+}
 
+class HW2P1 extends FlatSpec with Matchers {
+  // Write a recursive function to get the nth Fibonacci number (http://mng.bz/C29s).
+  //                                                                 The first two Fibonacci numbers are 0 and 1 . The nth number is always the sum of the
+  //                                                               previous two—the sequence begins 0, 1, 1, 2, 3, 5 . Your definition should use a
+  //                                                               local tail-recursive function.
+  //                                                               def fib(n: Int): Int
   "fib" should "give fib numbers" in {
     HOF.fib(4) should be (3)
     HOF.fib(3) should be (2)
